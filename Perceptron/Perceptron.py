6c924d4d01f7d64540fe1864b4129dc9dab5f40e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-18 08:30:19 +0530

    Update Perceptron.py

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -5,3 +5,24 @@ import numpy as np
 import tensorflow as tf
 from sklearn.metrics import roc_auc_score, accuracy_score
 s = tf.InteractiveSession()
+
+# Importing the Dataset and splitting into Train and Test
+
+def load_dataset(flatten=False):
+    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+	
+	# normalize x
+    X_train = X_train.astype(float) / 255
+    X_test = X_test.astype(float) / 255
+	
+    # we reserve the last 10000 training examples for validation
+    X_train, X_val = X_train[:-10000], X_train[-10000:]
+    y_train, y_val = y_train[:-10000], y_train[-10000:]
+    
+    if flatten:
+        X_train = X_train.reshape([X_train.shape[0], -1])
+       	X_val = X_val.reshape([X_val.shape[0], -1])
+        X_test = X_test.reshape([0], -1)
+    
+		
+    return X_train, y_train, X_val, y_val, X_test, y_test
