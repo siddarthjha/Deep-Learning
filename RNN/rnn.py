644e4d98dc376b80063714b135525e0b6f2ca6d7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-23 11:42:41 +0530

    Update rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -112,3 +112,9 @@ model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
 history = model.fit(train_dataset, epochs=10,
                     validation_data=test_dataset,
                     validation_steps=30)
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
+# predict on a sample text without padding.
