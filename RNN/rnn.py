eec3a2ef360c5c733239275488802d3f94b2118a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-23 11:42:22 +0530

    Update rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -104,3 +104,11 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(1)
 ])
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+history = model.fit(train_dataset, epochs=10,
+                    validation_data=test_dataset,
+                    validation_steps=30)
