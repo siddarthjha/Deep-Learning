bb904bc64a3c10eebbfdcd46a6dea3e9a7bddb79
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-23 11:41:57 +0530

    Update rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -95,3 +95,12 @@ print(predictions)
 plot_graphs(history, 'accuracy')
 plot_graphs(history, 'loss')
 
+# Stack two or more LSTM layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1)
+])
