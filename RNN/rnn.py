37f57de6f43be2be4ffc43ccafcd8332900a6bd7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-23 11:12:02 +0530

    Update rnn.py

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -45,3 +45,12 @@ BATCH_SIZE = 64
 train_dataset = train_dataset.shuffle(BUFFER_SIZE)
 train_dataset = train_dataset.padded_batch(BATCH_SIZE)
 test_dataset = test_dataset.padded_batch(BATCH_SIZE)
+
+# Create the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
