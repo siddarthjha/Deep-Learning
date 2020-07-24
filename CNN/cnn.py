637d55fa11eb8e83b67e8417dc5935c616c730de
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-24 21:45:32 +0530

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -83,3 +83,7 @@ model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',
                  activation ='relu'))
 model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
 model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
