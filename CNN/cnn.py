4c271c07b7b37a7715ed867005a23eedefa8eefa
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-24 21:49:36 +0530

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -116,3 +116,12 @@ history = model.fit_generator(datagen.flow(X_train,Y_train, batch_size=batch_siz
                               epochs = epochs, validation_data = (X_val,Y_val),
                               verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
                               , callbacks=[learning_rate_reduction])
+# Plot the loss and accuracy curves for training and validation 
+fig, ax = plt.subplots(2,1)
+ax[0].plot(history.history['loss'], color='b', label="Training loss")
+ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
+ax[1].plot(history.history['val_acc'], color='r',label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
