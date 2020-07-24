1dda235d6f7f83f93058944ee3647b302473f048
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-24 21:43:20 +0530

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -59,3 +59,6 @@ test = test.values.reshape(-1,28,28,1)
 # Encode labels to one hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
 Y_train = to_categorical(Y_train, num_classes = 10)
 # Set the random seed
+random_seed = 2
+# Split the train and the validation set for the fitting
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
