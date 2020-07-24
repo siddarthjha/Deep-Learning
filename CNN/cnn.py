49bfbac22d80ce0349bfb5ef3955e800cea0693c
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-24 20:08:18 +0530

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,2 +1,6 @@
-# This is a 5 layers Sequential Convolutional Neural Network for digits recognition trained on MNIST dataset.
+"""
 
+This is a 5 layers Sequential Convolutional Neural Network for digits recognition trained on MNIST dataset.
+Firstly, I will prepare the data (handwritten digits images) then i will focus on the CNN modeling and evaluation.
+
+"""
