73c8a301cd352b2731e5f185308c48a7cb43aa70
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-24 22:00:14 +0530

    Update cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -185,3 +185,20 @@ def display_errors(errors_index,img_errors,pred_errors, obs_errors):
             ax[row,col].imshow((img_errors[error]).reshape((28,28)))
             ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
             n += 1
+# Probabilities of the wrong predicted numbers
+Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+
+# Predicted probabilities of the true values in the error set
+true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+
+# Difference between the probability of the predicted label and the true label
+delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+
+# Sorted list of the delta prob errors
+sorted_dela_errors = np.argsort(delta_pred_true_errors)
+
+# Top 6 errors 
+most_important_errors = sorted_dela_errors[-6:]
+
+# Show the top 6 errors
+display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
